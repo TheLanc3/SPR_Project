54a14871dffb608c31aad04cc2c0296b108fffc6
thelanc3 <kmivanin@gmail.com>
2025-11-01 13:04:18 +0300

    Finished order service

--- src/services/order_service.go
+++ src/services/order_service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"fmt"
+	"spr-project/enums"
 	"spr-project/models"
 	"spr-project/parameters"
 	"spr-project/repositories"
@@ -12,6 +13,11 @@ import (
 
 type OrderService struct {
 	dB *gorm.DB
+}
+
+func NewOrderService(db *gorm.DB) *OrderService {
+	repo := OrderService{dB: db}
+	return &repo
 }
 
 func (service *OrderService) CreateNewOrder(ctx context.Context,
@@ -45,4 +51,65 @@ func (service *OrderService) CreateNewOrder(ctx context.Context,
 	}
 
 	return &result, nil
+}
+
+func (service *OrderService) UpdateOrderStatus(ctx context.Context,
+	orderId int64, status enums.Status) error {
+	err := service.dB.Transaction(func(tx *gorm.DB) error {
+		orderRepo := repositories.NewOrderRepository(tx)
+		if err := orderRepo.UpdateStatus(ctx, status, orderId); err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	return err
+}
+
+func (service *OrderService) GetOrdersByCustomer(ctx context.Context,
+	customerId int64, filter enums.OrderType) ([]models.Order, error) {
+	var orders []models.Order
+
+	err := service.dB.Transaction(func(tx *gorm.DB) error {
+		repo := repositories.NewOrderRepository(tx)
+
+		switch filter {
+		case enums.All:
+			{
+				data, err := repo.GetOrdersByCustomer(ctx, customerId, 5)
+				if err != nil {
+					return err
+				}
+				orders = data
+				break
+			}
+		case enums.OnlyUnfinished:
+			{
+				data, err := repo.GetUnfinishedOrdersByCustomer(ctx, customerId, 5)
+				if err != nil {
+					return err
+				}
+				orders = data
+				break
+			}
+		case enums.OnlyDelivered:
+			{
+				data, err := repo.GetDeliveredOrdersByCustomer(ctx, customerId, 5)
+				if err != nil {
+					return err
+				}
+				orders = data
+				break
+			}
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return []models.Order{}, err
+	}
+
+	return orders, nil
 }
