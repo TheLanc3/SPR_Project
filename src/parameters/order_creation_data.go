d29700fb29844f7019eb816051810787d4e52605
thelanc3 <kmivanin@gmail.com>
2025-11-11 19:16:52 +0300

    Enhanced NewOrderCreationData()

--- src/parameters/order_creation_data.go
+++ src/parameters/order_creation_data.go
@@ -11,7 +11,7 @@ func NewOrderCreationData(customerId int64,
 	var total int
 
 	for _, position := range positions {
-		total += position.Quantity
+		total += position.Quantity * position.Price
 	}
 
 	return OrderCreationData{customerId, total, positions}
